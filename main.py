070fa102796e5ae45cb769dd54c863f68b98fa9a
Chad Liberty <c1liberty@student.bridgew.edu>
2020-02-03 20:09:34 -0500

    project before tests, with readme

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+def main():
+    url ='https://jobs.github.com/positions.json'
+    request = requests.get(url)
+
+    print(request.status_code)
+    page_response = request.json()
+
+    #empty list to store dictionaries
+    response_dict = []
+    search_jobs(page_response)
+
+def search_jobs(page_response):
+    for job_listings in page_response[:30]:
+        url = "https://jobs.github.com/positions?page={}.json".format(job_listings)
+        request = requests.get(url)
+        print("id: {}\tstatus: {}".format(job_listings, request.status_code))
+
+main()
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+1. Chad Liberty
+2. no special install directions
+3. project makes API call, prints status of response. An empty list is set up to store dictionaries. Job posting id's are listed
+4. I do not think it will go through all 226 jobs and I only have two functions
