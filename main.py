a61b142b143570d38f836f1e6c19717a5b0c1daf
TheDoomForce <malkite1@auxilioabalto.me>
2020-02-08 13:19:59 -0500

    remove infinite recursion and fix smoe flak8 violations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def main():
     url ='https://jobs.github.com/positions.json'
     request = requests.get(url)
@@ -7,9 +8,10 @@ def main():
     print(request.status_code)
     page_response = request.json()
 
-    #empty list to store dictionaries
+    # empty list to store dictionaries
     response_dict = []
     search_jobs(page_response)
+
 
 def search_jobs(page_response):
     for job_listings in page_response[:30]:
@@ -18,10 +20,14 @@ def search_jobs(page_response):
         print("id: {}\tstatus: {}".format(job_listings, request.status_code))
 
 main()
-#this was supposed to write/save the info to a jobListing text file
+
+
+# this was supposed to write/save the info to a jobListing text file
 def save_file(request):
     file = open("jobListing.txt")
     for i in range(200):
         file.write(request)
-    save_file()
-    save_file(request)
+    file.close()
+    # remove infinite recursion
+    # save_file()
+    # save_file(request)
