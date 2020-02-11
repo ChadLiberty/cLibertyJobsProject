fe1b265d511b9a3068e045ccca3d479ea259f621
Chad Liberty <c1liberty@student.bridgew.edu>
2020-02-11 15:11:59 -0500

    project with tests and readme

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import requests
+import time
+from typing import Dict, List
+import sqlite3
+
+def get_github_jobs_data() -> List[Dict]:
+    """retrieve github jobs data in form of a list of dictionaries after json processing"""
+    all_data = []
+    page = 1
+    more_data = True
+    while more_data:
+        url = f"https://jobs.github.com/positions.json?page={page}"
+        raw_data = requests.get(url)
+        if "GitHubber!" in raw_data:  # sometimes if I ask for pages too quickly I get an error; only happens in testing
+            continue  # trying continue, but might want break
+        partial_jobs_list = raw_data.json()
+        all_data.extend(partial_jobs_list)
+        if len(partial_jobs_list) < 50:
+            more_data = False
+        time.sleep(.1)  # short sleep between requests so I dont wear out my welcome.
+        page += 1
+    return all_data
+
+def save_data(data, filename='data.txt'):
+    with open(filename, 'a') as file:
+        for item in data:
+            print(item, file=file)
+
+def save_to_db(data):
+    """:keyword data is a list of dictionaries. Each dictionary is a JSON object with a bit of jobs data"""
+    pass
 
 def main():
-    url ='https://jobs.github.com/positions.json'
-    request = requests.get(url)
+    data = get_github_jobs_data()
+    save_data(data)
 
-    print(request.status_code)
-    page_response = request.json()
+if __name__ == '__main__':
+    main()
 
-    #empty list to store dictionaries
-    response_dict = []
-    search_jobs(page_response)
-
-def search_jobs(page_response):
-    for job_listings in page_response[:30]:
-        url = "https://jobs.github.com/positions?page={}.json".format(job_listings)
-        request = requests.get(url)
-        print("id: {}\tstatus: {}".format(job_listings, request.status_code))
-
-main()
-#this was supposed to write/save the info to a jobListing text file
-def save_file(request):
-    file = open("jobListing.txt")
-    for i in range(200):
-        file.write(request)
-    save_file()
-    save_file(request)
